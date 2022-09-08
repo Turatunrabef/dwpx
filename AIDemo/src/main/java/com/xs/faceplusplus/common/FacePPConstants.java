0392650a249de962d0b7795aa82e62040a667768
Turatunrabef <pangjivr90017@yeah.net>
2022-09-08 08:03:30 +0800

    :sparkles:faceplusplus 常量对象

---
+++ AIDemo/src/main/java/com/xs/faceplusplus/common/FacePPConstants.java
@@ -0,0 +1,11 @@
+package com.xs.faceplusplus.common;
+/**
+ * FacePlusPlus 应用常量对象
+ * @author 小帅丶
+ *
+ */
+public class FacePPConstants {
+	public static final String API_KEY = "";//应用apikey
+	public static final String API_SECRET = "";//应用apisecret
+	public static final String FACE_DETECT_URL = "https://api-cn.faceplusplus.com/facepp/v3/detect";//人脸检测接口地址
+}
