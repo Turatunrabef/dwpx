2da1d836e4d131c0f910e9d2ea3a0633e4633eb4
Turatunrabef <pangjivr90017@yeah.net>
2022-09-16 05:46:10 +0800

    :sparkles:FPP人脸肤质分析示例代码

--- AIDemo/src/main/java/com/xs/faceplusplus/FaceSkinSample.java
+++ AIDemo/src/main/java/com/xs/faceplusplus/FaceSkinSample.java
@@ -1,15 +1,16 @@
 package com.xs.faceplusplus;
 
+import java.net.URLEncoder;
 import java.util.Base64;
 import java.util.Base64.Encoder;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.alibaba.fastjson.JSON;
-import com.xiaoleilu.hutool.http.HttpUtil;
 import com.xs.faceplusplus.bean.FacePPBean;
 import com.xs.faceplusplus.common.FacePPConstants;
 import com.xs.util.baidu.FileUtil;
+import com.xs.util.baidu.HttpUtil;
 /**
  * FacePlusPlus 人脸检测示例代码
  * @author 小帅丶
@@ -17,19 +18,14 @@ import com.xs.util.baidu.FileUtil;
  */
 public class FaceSkinSample {
 	public static void main(String[] args) throws Exception {
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("api_key", FacePPConstants.API_KEY);
-		params.put("api_secret", FacePPConstants.API_SECRET);
 		
 		Encoder base64Encoder = Base64.getEncoder();
 		String image_base64 = base64Encoder.encodeToString(FileUtil.readFileByBytes("G:/testimg/zdx.jpg"));
 		String return_attributes = "gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus";
 		
-		params.put("image_base64", image_base64);
-		params.put("return_attributes", return_attributes);
-		
+		String params = "api_key="+FacePPConstants.API_KEY+"&api_secret="+FacePPConstants.API_SECRET+"&image_base64="+URLEncoder.encode(image_base64,"utf-8")+"&return_attributes="+return_attributes;
 		String result = HttpUtil.post(FacePPConstants.FACE_DETECT_URL, params);
-		
+		System.out.println(result);
 		FacePPBean facePP = JSON.parseObject(result, FacePPBean.class);
 		if(facePP.getFaces().size()>0){
 			System.out.println("肤质健康分数:"+facePP.getFaces().get(0).getAttributes().getSkinstatus().getHealth());
